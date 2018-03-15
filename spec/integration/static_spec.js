12f6d22cb213aca64aab39821203009ac961a44f
Matthew Jackson <Matthew@Matthew-Jacksons-MacBook-Pro.local>
2018-03-15 23:54:35 +0000

    bloccit-1: setup complete

---
+++ spec/integration/static_spec.js
@@ -0,0 +1,20 @@
+const request = require("request");
+const server = require("../../src/server");
+const base = "http://localhost:3000/";
+
+describe("routes : static", () => {
+
+//#1
+    describe("GET /", () => {
+
+//#2
+    it("should return status code 200", () => {
+
+//#3
+      request.get(base, (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+        });
+      });
+
+    });
+  });
