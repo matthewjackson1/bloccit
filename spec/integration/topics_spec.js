1327d5519a76904ff9101f6a527a758675346fa6
Matthew Jackson <matthewj91@hotmail.com>
2018-03-30 17:52:52 +0100

    bloccit-11 setup complete

--- spec/integration/topics_spec.js
+++ spec/integration/topics_spec.js
@@ -4,6 +4,7 @@ const base = "http://localhost:3000/topics/";
 
 const sequelize = require('../../src/db/models/index').sequelize;
 const Topic = require("../../src/db/models").Topic;
+const User = require("../../src/db/models").User;
 
 describe("routes : topics", () => {
 
@@ -28,14 +29,23 @@ describe("routes : topics", () => {
   // context of admin user
   describe("admin user performing CRUD actions for Topic", () => {
 
-    beforeEach((done) => {  // before each suite in admin context
-      request.get({         // mock authentication
-        url: "http://localhost:3000/auth/fake",
-        form: {
-          role: "admin"     // mock authenticate as admin user
-        }
+    beforeEach((done) => {
+      User.create({
+        email: "admin@example.com",
+        password: "123456",
+        role: "admin"
+      })
+      .then((user) => {
+        request.get({         // mock authentication
+          url: "http://localhost:3000/auth/fake",
+          form: {
+            role: user.role,     // mock authenticate as admin user
+            userId: user.id,
+            email: user.email
+          }
+        });
+        done();
       });
-      done();
     });
 
     describe("GET /topics", () => {
